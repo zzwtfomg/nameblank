45b7e17e7c0fbbdb51bc2b1ae4880fd985b48903
Z z <zz@169-231-196-239.wireless.ucsb.edu>
2024-10-03 13:00:29 -0700

    backgroundgallery2

--- src/Gallery.tsx
+++ src/Gallery.tsx
@@ -12,7 +12,7 @@ function Gallery() {
       </header>
       <div 
         className="flex flex-col items-center justify-center min-h-screen pt-16 bg-cover bg-center bg-no-repeat"
-        style={{ backgroundImage: "url('/col1/img11.jpg')" }}
+        style={{ backgroundImage: "url('/img11.jpg')" }}
       >
         <div className="text-center max-w-2xl menlo font-light bg-black bg-opacity-70 p-6 rounded-lg">
           <p className="text-lg mb-3 text-white">Coming Soon!</p>
