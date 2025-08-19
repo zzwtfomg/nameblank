54b22b7f53d0e862c46958cecca40b9b0a8e8432
zz <zafeinglass@gmail.com>
2025-08-19 11:31:58 -0700

    added my name in about

--- src/About.tsx
+++ src/About.tsx
@@ -8,7 +8,7 @@ function About() {
       <Header />
       <div className="flex flex-col items-center justify-center min-h-screen pt-24 p-10">
         <div className="text-center max-w-2xl menlo font-light">
-          <p className="text-lg mb-8">I'm from West Hollywood, CA and I like to make clothes that actually feel like something. Two years ago I realized there was nothing I owned or could buy that felt like me, so I taught myself how to sew and pattern draft.</p>
+          <p className="text-lg mb-8">Hi I'm Zeke and I like to make clothes that actually feel like something. Two years ago I realized there was nothing I owned or could buy that felt like me, so I taught myself how to sew and pattern draft.</p>
           <p className="text-lg mb-8">So often when you buy clothes you think of how others would see you in them or how they've looked on others, and each piece is put into a category. There's words like 'style', 'fit', 'gender', 'season', and certainly many more that lead to clothes being thought of in an unhealthy way. I don't need someone telling me how to wear MY clothes, and I'm sure many feel the same. </p>
           <p className="text-lg mb-8">As such, all my clothes are unisex and are meant to be tailored to any given person's unique expression. </p>
         </div>
